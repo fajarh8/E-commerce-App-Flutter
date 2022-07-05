ab6098a9db72c5ebf6153136029ffa580ef4df2d
fajarh8 <fajar0heryan@gmail.com>
2022-07-05 11:56:06 +0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF535353);
+const mainColor = Color(0xFF3D82AE);
+const kTextLightColor = Color(0xFFACACAC);
+
+const kDefaultPaddin = 20.0;
